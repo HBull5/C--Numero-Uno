a8f81650588d9e87a6f6b07bbe261b6edfea1f31
Harcourt <harcourtbullv@gmail.com>
2021-07-02 11:31:12 -0400

    init commit

---
+++ NumberGuesster/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesster
+{
+
+    // Main Class
+    class Program
+    {
+        // Entry Point Like in Java
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
